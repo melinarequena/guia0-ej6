f472690fd6643e99edb00f45f9f07009d25dad26
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 13:47:17 -0300

    Ej 7

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+/* Guia 0, Ej 6
+ Escriba una función que calcule la suma y el producto de dos números enteros
+utilizando punteros y luego los muestre por pantalla */
 
-int main() {
-    printf("Hello, World!\n");
+#include<stdio.h>
+void op(int *, int *);
+
+int main(){
+    int a, b, *aptr, *bptr;
+    a = 2;
+    b = 3;
+    aptr = &a;
+    bptr=  &b;
+    printf("Los numeros a y b son: %d y %d\n", a, b);
+    op(aptr, bptr);
+
     return 0;
+}
+
+void  op(int *aptr, int *bptr){
+    printf("La suma es %d\n", *aptr + *bptr);
+    printf("El producto es %d\n", *aptr * *bptr);
 }
